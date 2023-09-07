2db462b63d2027b6b9da62eef48029a52bebbd7c
Harold <wshwilfried@gmail.com>
2023-09-07 13:57:10 -0500

    Added Test Case ID: T42

--- ctam/interfaces/telemetry_ifc.py
+++ ctam/interfaces/telemetry_ifc.py
@@ -12,6 +12,7 @@ import ast
 from datetime import datetime
 from ocptv.output import LogSeverity
 from utils.json_utils import *
+from itertools import product
 
 class TelemetryIfc(FunctionalIfc):
 
@@ -664,6 +665,35 @@ class TelemetryIfc(FunctionalIfc):
         reference_uri = r"/redfish/v1/Chassis/{ChassisFpgaIDs}"
         for uri in systemchassis_instances:
             uri = "/Chassis/" + uri
+            base_uri = self.dut().uri_builder.format_uri(redfish_str="{BaseURI}", component_type="GPU")
+            chassis_uri = base_uri + uri
+            response = self.dut().run_redfish_command(uri=chassis_uri)
+            JSONData = response.dict
+            response_check = self.dut().check_uri_response(reference_uri, JSONData)
+            msg = "Checking for redfish uri for Accelerator Compliance, Result : {}".format( response_check)            
+            self.write_test_info(msg)
+            status = response.status
+            if (status == 200 or status == 201) and response_check:
+                self.test_run().add_log(LogSeverity.INFO, "Test JSON")
+                self.test_run().add_log(LogSeverity.INFO, "Chassis with ID Pass: {} : {}".format(uri, json.dumps(JSONData, indent=4)))
+            else:
+                self.test_run().add_log(LogSeverity.FATAL, "Chassis with ID Fails: {} : {}".format(uri, JSONData))
+                result = False
+        return result
+    
+    def ctam_get_chassis_fpga_sensor_metrics(self):
+        """
+        :Description:				Read back the data of /redfish/v1/Chassis/{ChassisFpgaIDs}/Sensors
+
+        :returns:				    Dictionary record under of all URIs under /redfish/v1/Chassis/{ChassisFpgaIDs}/Sensors
+        """
+        MyName = __name__ + "." + self.ctam_get_chassis_fpga_sensor_metrics.__qualname__
+        chassis_sensor_list = ast.literal_eval(self.dut().uri_builder.format_uri(redfish_str="{ChassisSensorID}", component_type="GPU"))
+        chassis_fpga_list = ast.literal_eval(self.dut().uri_builder.format_uri(redfish_str="{ChassisFpgaIDs}", component_type="GPU"))
+        result = True
+        reference_uri = r"/redfish/v1/Chassis/{ChassisFpgaIDs}/Sensors"
+        for sensorInstance,fpgaInstance in product(chassis_sensor_list, chassis_fpga_list):
+            uri = "/Chassis/" + fpgaInstance + "/Sensors/" + sensorInstance
             base_uri = self.dut().uri_builder.format_uri(redfish_str="{BaseURI}", component_type="GPU")
             chassis_uri = base_uri + uri
             response = self.dut().run_redfish_command(uri=chassis_uri)
