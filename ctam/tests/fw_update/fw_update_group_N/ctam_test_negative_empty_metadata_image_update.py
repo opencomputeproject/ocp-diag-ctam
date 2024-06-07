b4ca255d8abdad54bb28221d9f2911d2ed2a495d
v-sourabhsingh <v-sourasingh@microsoft.com>
2024-06-07 18:34:57 +0530

    Comments added for teardown Ac Power Cycle

--- ctam/tests/fw_update/fw_update_group_N/ctam_test_negative_empty_metadata_image_update.py
+++ ctam/tests/fw_update/fw_update_group_N/ctam_test_negative_empty_metadata_image_update.py
@@ -123,10 +123,10 @@ class CTAMTestNegativeEmptyMetadataImageUpdate(TestCase):
         step2 = self.test_run().add_step(f"{self.__class__.__name__} run(), step2")
         with step2.scope():
             if self.group.fw_update_ifc.ctam_activate_ac(gpu_check=False, fwupd_hyst_wait=False):
-                msg = f"{self.test_id} : AC Cycle Passed"
+                msg = f"{self.test_id} : Teardown : AC Cycle Passed"
                 self.test_run().add_log(LogSeverity.DEBUG, msg)  
             else:
-                msg = f"{self.test_id} : AC Cycle Failed"
+                msg = f"{self.test_id} : Teardown : AC Cycle Failed"
                 self.test_run().add_log(LogSeverity.DEBUG, msg)
         
         # call super teardown last
