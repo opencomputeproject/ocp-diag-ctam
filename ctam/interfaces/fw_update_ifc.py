d83c33460fae6dd3965a3be6cdcd977d2d5c80e5
Dhruv Rathi <dhruvr@nvidia.com>
2025-02-24 08:08:08 -0800

    Moving check for element in exclude_list out of the negative testcase senarios

--- ctam/interfaces/fw_update_ifc.py
+++ ctam/interfaces/fw_update_ifc.py
@@ -131,11 +131,12 @@ class FWUpdateIfc(FunctionalIfc, metaclass=Meta):
                             if not Package_Version:
                                 msg += "Not in the PLDM bundle"
                             
+                            elif element["Id"] in self.dut().redfish_uri_config.get("GPU_FWUpdate", {}).get("exclude_targets_list", []):
+                                msg += "Skipping as it is in the exclude list"
+                            
                             elif element["Version"] not in Package_Version and (
                                 self.included_targets == []
                                 or element["@odata.id"] in self.included_targets
-                            ) and (
-                                element["Id"] not in self.dut().redfish_uri_config.get("GPU_FWUpdate", {}).get("exclude_targets_list", [])
                             ):
                                 VersionsDifferent = False
                                 msg += f"Update Capable to {Package_Version}"
@@ -323,13 +324,16 @@ class FWUpdateIfc(FunctionalIfc, metaclass=Meta):
         # Verify version of components currently reporting in FW inventory
         for element in self.PostInstallDetails:
             try:
+                if element["Id"] in self.dut().redfish_uri_config.get("GPU_FWUpdate", {}).get("exclude_targets_list", []):
+                    msg = f"Skipping {element['Id']} as it is in the exclude list"
+                    self.test_run().add_log(LogSeverity.DEBUG, msg)
+                    continue
                 negative_case = (
                     image_type == "negate" 
                     or str(element["Updateable"]).lower() == "false" # Note, this may mean empty SoftwareId. So this condition needs to come before the next one
                     or (image_type == "corrupt_component" and int(element["SoftwareId"], 16) == int(corrupted_component_id, 16) )
                     or (self.included_targets != []
                         and element["@odata.id"] not in self.included_targets)
-                    or element["Id"] in self.dut().redfish_uri_config.get("GPU_FWUpdate", {}).get("exclude_targets_list", [])
                 )
                 if negative_case:
                     # FW version should be same as from pre-update
