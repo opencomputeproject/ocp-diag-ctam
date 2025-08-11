b478212c711a28cf2efaa11bd0f34b136b46aab5
dhruvr <dhruvr@nvidia.com>
2025-08-11 20:14:00 +0530

    Updated H4 testcase, added UpdateService.json

--- ctam/tests/health_check/basic_health_check_group/ctam_test_redfish_update_service.py
+++ ctam/tests/health_check/basic_health_check_group/ctam_test_redfish_update_service.py
@@ -22,6 +22,8 @@ LICENSE file in the root directory of this source tree.
 """
 from typing import Optional, List
 from tests.test_case import TestCase
+from test_hierarchy import TestHierarchy
+import os
 from ocptv.output import (
     DiagnosisType,
     LogSeverity,
@@ -32,6 +34,7 @@ from ocptv.output import (
 from tests.health_check.basic_health_check_group.basic_health_check_test_group import (
     BasicHealthCheckTestGroup,
 )
+from utils.ctam_utils import GitUtils
 
 
 class CTAMTestRedfishUpdateService(TestCase):
@@ -45,8 +48,8 @@ class CTAMTestRedfishUpdateService(TestCase):
     test_name: str = "CTAM Test Redfish Update Service"
     test_id: str = "H4"
     score_weight: int = 10
-    tags: List[str] = ["HCheck", "L3"]
-    compliance_level: str = "L3"
+    tags: List[str] = ["HCheck", "L2"]
+    compliance_level: str ="L2"
 
     # exclude_tags: List[str] = ["NotCheck"]
 
@@ -56,6 +59,7 @@ class CTAMTestRedfishUpdateService(TestCase):
         """
         super().__init__()
         self.group = group
+        self.git_utils = GitUtils()
 
     def setup(self):
         """
@@ -73,17 +77,39 @@ class CTAMTestRedfishUpdateService(TestCase):
         """
         actual test verification
         """
-        result = True
         failure_reason = ""
+        result = True
+        logger_path = os.path.join(self.dut().logger_path, "RedfishInteropValidator", f"{self.__class__.test_id}_{self.__class__.__name__}")
+
+        #cloning Redfish Interop Validator under temp folder which will be deleted after completion of test case.
         step1 = self.test_run().add_step(f"{self.__class__.__name__} run(), step1")  # type: ignore
         with step1.scope():
-            JSONData = self.group.health_check_ifc.ctam_getus()
-            if JSONData is None or "error" in JSONData:
-                step1.add_log(LogSeverity.ERROR, f"{self.test_id} : Redfish Update Service Check - Failed")
-                failure_reason += "Redfish Update Service Check - Failed"
-                result = False
-            else:
-                step1.add_log(LogSeverity.INFO, f"{self.test_id} : Redfish Update Service Check - Completed")
+            result = self.git_utils.clone_repo(repo_url="https://github.com/microsoft/Redfish-Interop-Validator.git",
+                                  repo_path="Redfish-Interop-Validator")
+
+        if result:
+            step2 = self.test_run().add_step(f"{self.__class__.__name__} run(), step2")  # type: ignore
+            with step2.scope():
+                # Change this JSON file name and path if you want to run any other excel.
+                source_json = "UpdateService.json"
+                json_file_path = os.path.join(self.dut().default_config_path, source_json)
+                #Running RedfishInteropValidator using json profiles.
+                file_name="RedfishInteropValidator"
+                base_uri = self.dut().uri_builder.format_uri(redfish_str="{GPUMC}",
+                                                                    component_type="GPU")
+                repo_file_name = os.path.join(self.git_utils.repo_path, file_name)
+
+                result,error_count = GitUtils.ctam_redfish_interop_validator(file_name=repo_file_name, connection_url=self.dut().connection_url,
+                                                user_name=self.dut().user_name, user_pass=self.dut().user_pass,
+                                                log_path=logger_path, passthrough=base_uri,
+                                                payload="NodeTree /redfish/v1/UpdateService",
+                                                profile=json_file_path,
+                                                )
+                if not result:
+                    step2.add_log(LogSeverity.ERROR, f"Validation has failed: {error_count} problems found")
+                    failure_reason += f"Validation has failed: {error_count} problems found"
+                else:
+                    step2.add_log(LogSeverity.INFO, f"Redfish Service Command ran successfully and validated.")
 
         # ensure setting of self.result and self.score prior to calling super().run()
         self.result = TestResult.PASS if result else TestResult.FAIL
@@ -101,7 +127,8 @@ class CTAMTestRedfishUpdateService(TestCase):
         # add custom teardown here
         step1 = self.test_run().add_step(f"{self.__class__.__name__}  teardown()...")
         with step1.scope():
-            pass
+            step1.add_log(LogSeverity.INFO, f"Cleaning repo after running Redfish Interop Validator.")
+            self.git_utils.clean_repo()
 
         # call super teardown last
         super().teardown()
